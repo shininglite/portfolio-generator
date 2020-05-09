17382bcb843b2052db11edd41c868869ee4d6215
frunox <60527588+frunox@users.noreply.github.com>
2020-05-09 11:01:50 -0500

    added basic components

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,14 @@ import React, { Fragment } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { Container } from 'semantic-ui-react';
 
-import UserForm from './components/UserForm';
+import Developer from './pages/Developer';
+
+// import UserForm from './components/UserForm';
 // import "./App.css";
 
 const App = () => (
   <Fragment>
-    <Container>
-
-      <UserForm />
-    </Container>
+    <Developer />
   </Fragment>
 );
 
