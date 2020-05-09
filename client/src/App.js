d399b0f1c7e9977b0d1e2af628aaadf2590d544b
frunox <60527588+frunox@users.noreply.github.com>
2020-05-08 21:31:11 -0500

    start ui creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Fragment } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import UserForm from './components/UserForm';
+// import "./App.css";
+
+const App = () => (
+  <Fragment>
+    <Container>
+
+      <UserForm />
+    </Container>
+  </Fragment>
+);
+
 
 export default App;
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,75 +1,25 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./style.css";
+import React from 'react';
+import { Container, Image, Menu } from 'semantic-ui-react';
 
-class Nav extends Component {
-  state = {
-    open: false,
-    width: window.innerWidth,
-  };
+export default () => (
+  <Menu>
+    <Container>
+      <Menu.Item as="a" header>
+        <Image
+          size="small"
+          src="/img/page/logo.svg"
+        />
+      </Menu.Item>
 
-  updateWidth = () => {
-    const newState = { width: window.innerWidth };
+      <Menu.Menu position="right">
+        <Menu.Item as="a" name="login">
+          Login
+        </Menu.Item>
 
-    if (this.state.open && newState.width > 991) {
-      newState.open = false;
-    }
-
-    this.setState(newState);
-  };
-
-  toggleNav = () => {
-    this.setState({ open: !this.state.open });
-  };
-
-  componentDidMount() {
-    window.addEventListener("resize", this.updateWidth);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateWidth);
-  }
-
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
-        <Link className="navbar-brand" to="/">
-          Home
-        </Link>
-        <button
-          onClick={this.toggleNav}
-          className="navbar-toggler"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div
-          className={`${this.state.open ? "" : "collapse "}navbar-collapse`}
-          id="navbarNav"
-        >
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link
-                onClick={this.toggleNav}
-                className={
-                  window.location.pathname === "/"
-                    ? "nav-link active"
-                    : "nav-link"
-                }
-                to="/"
-              >
-                Developer
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-}
-
-export default Nav;
+        <Menu.Item as="a" name="register">
+          Register
+        </Menu.Item>
+      </Menu.Menu>
+    </Container>
+  </Menu>
+);
