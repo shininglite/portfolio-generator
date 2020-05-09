7147e06e5273a0428d0d58419998bddf178ecac4
frunox <60527588+frunox@users.noreply.github.com>
2020-05-08 21:32:39 -0500

    start ui development

--- test.js
+++ test.js
@@ -4,84 +4,85 @@ let axios = require("axios");
 // let testRoute = require("./routes");
 
 function updateDevDB(developerLoginName) {
-  // axios
-  //   .get("/api/developer/updateDevDB", {
-  //     params: { developerLoginName: developerLoginName },
-  //   })
-  //   .then((data) => console.log(data))
-  //   .catch((err) => console.log(err));
-  var gitHubData;
-  axios
-    .get(
-      "https://api.github.com/search/repositories?q=user:" + developerLoginName
-    )
-    .then((data) => {
-      gitHubData = data;
-    })
-    .then(() => {
-      return db.Developer.findOne({ developerLoginName: developerLoginName });
-    })
-    .then((devData) => {
-      loadDB(devData, gitHubData);
-    })
-    .catch((err) => console.log(err));
+    // axios
+    //   .get("/api/developer/updateDevDB", {
+    //     params: { developerLoginName: developerLoginName },
+    //   })
+    //   .then((data) => console.log(data))
+    //   .catch((err) => console.log(err));
+    var gitHubData;
+    axios
+        .get(
+            "https://api.github.com/search/repositories?q=user:" + developerLoginName
+        )
+        .then((data) => {
+            gitHubData = data;
+        })
+        .then(() => {
+            return db.Developer.findOne({ developerLoginName: developerLoginName });
+        })
+        .then((devData) => {
+            loadDB(devData, gitHubData);
+        })
+        .catch((err) => console.log(err));
 }
 mongoose.connect("mongodb://localhost/portfolio_db", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
 
 function loadDB(devData, gitHubData) {
-  //
-  if (!gitHubData) {
-    return;
-  } else {
-    if (!devData) {
-      let devData = {
-        developerLoginName: gitHubData.data.items[0].owner.login,
-        developerGithubID: gitHubData.data.items[0].owner.id,
-      };
-      db.Developer.insertMany(devData);
+    //
+    if (!gitHubData) {
+        return;
+    } else {
+        if (!devData) {
+            let devData = {
+                developerLoginName: gitHubData.data.items[0].owner.login,
+                developerGithubID: gitHubData.data.items[0].owner.id,
+            };
+            db.Developer.insertMany(devData);
+        }
+
+        gitHubData.data.items.forEach((repo) => {
+            // db.Repository.findOne({ repoID: repo.id }).catch(
+
+            // )
+            updateRepo(repo, gitHubData.data.items[0].owner.id);
+        });
     }
-
-    gitHubData.data.items.forEach((repo) => {
-      // db.Repository.findOne({ repoID: repo.id }).catch(
-
-      // )
-      updateRepo(repo, gitHubData.data.items[0].owner.id);
-    });
-  }
 }
 
 function updateRepo(repo, devID) {
-  if (!repo.description) {
-    repo.description = repo.name;
-  }
-  let repoDevData = {
-    repoName: repo.name,
-    repoDesc: repo.description,
-    activeFlag: false,
-    deploymentLink: "",
-    html_url: repo.html_url,
-    repoID: repo.id,
-  };
-  db.Repository.insertMany(repoDevData).then((repoArray) => {
-    db.Developer.findOneAndUpdate(
-      { developerGithubID: devID },
-      {
-        $push: {
-          repositories: repoArray.map((element, key) => element._id),
-        },
-      },
-      { new: true }
-    ).catch((err) => {
-      console.error(err);
-      process.exit(1);
+    if (!repo.description) {
+        repo.description = repo.name;
+    }
+    let repoDevData = {
+        repoName: repo.name,
+        repoDesc: repo.description,
+        activeFlag: false,
+        deploymentLink: "",
+        html_url: repo.html_url,
+        repoID: repo.id,
+    };
+    db.Repository.insertMany(repoDevData).then((repoArray) => {
+        db.Developer.findOneAndUpdate(
+            { developerGithubID: devID },
+            {
+                $push: {
+                    repositories: repoArray.map((element, key) => element._id),
+                },
+            },
+            { new: true }
+        ).catch((err) => {
+            console.error(err);
+            process.exit(1);
+        });
     });
-  });
 }
 // updateDevDB("srfrog1970");
-updateDevDB("frunox");
+updateDevDB("srfrog1970");
 
 // async function getDev(devGithubID) {
 //   //
