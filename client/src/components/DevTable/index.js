0360ac8d4a3a4f57c85e73d2e87eb2b49ff69257
frunox <60527588+frunox@users.noreply.github.com>
2020-05-11 11:05:28 -0500

    test node module exclusion

--- client/src/components/DevTable/index.js
+++ client/src/components/DevTable/index.js
@@ -1,17 +1,12 @@
 // import axios from "axios";
 import _ from "lodash";
-import React, { Component } from "react";
-import { Table } from "semantic-ui-react";
+import React, { Component, Fragment } from "react";
+import { Table, Form } from "semantic-ui-react";
 import API from "../../utils/API";
 import './style.css'
 var id;
 
-let tableData = [
-  //     { name: 'Project 1', description: "Project description", activeFlag: 'true', activate: "Y" },
-  //     { name: 'Employee_Tracker', description: "This is a very long description intended to stretch past the end of the column.", activeFlag: 'false', activate: "X" },
-  //     { name: 'Code-Quiz', description: "Project description", activeFlag: 'false', activate: "X" },
-  //     { name: 'Personal_Library', description: "Project description", activeFlag: 'false', activate: "X" },
-]
+let tableData = []
 
 export default class DevTable extends Component {
   state = {
@@ -66,56 +61,94 @@ export default class DevTable extends Component {
     const { column, data, direction } = this.state;
 
     return (
-      <div className="devTable">
-        <Table sortable celled fixed singleLine>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell
-                width={4}
-                sorted={column === "name" ? direction : null}
-                onClick={this.handleSort("name")}
-              >
-                Repo Name
+      <Fragment>
+        <div className="devTable">
+          <Table sortable celled fixed singleLine>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell
+                  width={4}
+                  sorted={column === "name" ? direction : null}
+                  onClick={this.handleSort("name")}
+                >
+                  Repo Name
             </Table.HeaderCell>
-              <Table.HeaderCell
-                sorted={column === "description" ? direction : null}
-                onClick={this.handleSort("description")}
-              >
-                Description
+                <Table.HeaderCell
+                  sorted={column === "description" ? direction : null}
+                  onClick={this.handleSort("description")}
+                >
+                  Description
             </Table.HeaderCell>
-              <Table.HeaderCell
-                width={2}
-                textAlign="center"
-                sorted={column === "activeFlag" ? direction : null}
-                onClick={this.handleSort("activeFlag")}
-              >
-                Active
+                <Table.HeaderCell
+                  width={2}
+                  textAlign="center"
+                  sorted={column === "activeFlag" ? direction : null}
+                  onClick={this.handleSort("activeFlag")}
+                >
+                  Active
             </Table.HeaderCell>
-              <Table.HeaderCell
-                width={2}
-                textAlign="center"
-                sorted={column === "activate" ? direction : null}
-                onClick={this.handleSort("activate")}
-              >
-                Activate
+                <Table.HeaderCell
+                  width={2}
+                  textAlign="center"
+                  sorted={column === "activate" ? direction : null}
+                  onClick={this.handleSort("activate")}
+                >
+                  Activate
             </Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {_.map(
-              data,
-              ({ repoDesc, activeFlag, repoName, activate }, index) => (
-                <Table.Row key={index}>
-                  <Table.Cell>{repoName}</Table.Cell>
-                  <Table.Cell>{repoDesc}</Table.Cell>
-                  <Table.Cell textAlign="center">{activeFlag}</Table.Cell>
-                  <Table.Cell id={index} value={index} textAlign='center' selectable onClick={e => this.changeFlag(e.target.id)}>{activate}</Table.Cell>
-                </Table.Row>
-              )
-            )}
-          </Table.Body>
-        </Table >
-      </div >
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {_.map(
+                data,
+                ({ repoDesc, activeFlag, repoName, activate }, index) => (
+                  <Table.Row key={index}>
+                    <Table.Cell>{repoName}</Table.Cell>
+                    <Table.Cell>{repoDesc}</Table.Cell>
+                    <Table.Cell textAlign="center">{activeFlag}</Table.Cell>
+                    <Table.Cell id={index} value={index} textAlign='center' selectable onClick={e => this.changeFlag(e.target.id)}>{activate}</Table.Cell>
+                  </Table.Row>
+                )
+              )}
+            </Table.Body>
+          </Table >
+        </div >
+        <div className="formBox">
+          <Form inverted>
+            <Form.Group widths='equal'>
+              <Form.Field label='ActiveFlag Value: ' control='input' />
+              <Form.Field label='An HTML <select>' control='select'>
+                <option value='true'>True</option>
+                <option value='false'>False</option>
+              </Form.Field>
+            </Form.Group>
+            <Form.Group grouped>
+              <label>HTML radios</label>
+              <Form.Field
+                label='This one'
+                control='input'
+                type='radio'
+                name='htmlRadios'
+              />
+              <Form.Field
+                label='That one'
+                control='input'
+                type='radio'
+                name='htmlRadios'
+              />
+            </Form.Group>
+            <Form.Group grouped>
+              <label>HTML checkboxes</label>
+              <Form.Field label='This one' control='input' type='checkbox' />
+              <Form.Field label='That one' control='input' type='checkbox' />
+            </Form.Group>
+            <Form.Field label='An HTML <textarea>' control='textarea' rows='3' />
+            <Form.Field label='An HTML <button>' control='button'>
+              HTML Button
+          </Form.Field>
+          </Form>
+        </div>
+
+      </Fragment>
     );
   }
 }
