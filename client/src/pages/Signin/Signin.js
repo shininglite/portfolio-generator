e646feda2f6fb33ade71f96e3cf44da71cdcae33
Yeng Vang <yenghuevang@hotmail.com>
2020-05-12 10:48:20 -0500

    conditional render signin/create account test working

--- client/src/pages/Signin/Signin.js
+++ client/src/pages/Signin/Signin.js
@@ -3,13 +3,33 @@ import SigninComp from "../../components/SignIn"
 import CreateAccountComp from "../../components/CreateAccount"
 import "./Signin.css";
 
-function SignIn() {
-  return (
-    <div>
-    <SigninComp></SigninComp>
-    <CreateAccountComp></CreateAccountComp>
-    </div>
 
-  )
-}
+
+function SignIn() {
+  // return (
+  //   <div>
+  //   <SigninComp></SigninComp>
+  //   <CreateAccountComp></CreateAccountComp>
+  //   </div>
+
+  // )
+  let isLoggedIn = true;
+
+    if (isLoggedIn) {
+      return <div><SigninComp isLoggedIn={true}></SigninComp></div>
+    }
+    else {
+      return <div><CreateAccountComp></CreateAccountComp></div>
+    }
+  }
+
+  // return (
+  //   <div>
+  //     {isLoggedIn
+  //       ? <SigninComp></SigninComp>
+  //       : <CreateAccountComp></CreateAccountComp>
+  //     }
+  //   </div>
+  // )
+//}
 export default SignIn;
