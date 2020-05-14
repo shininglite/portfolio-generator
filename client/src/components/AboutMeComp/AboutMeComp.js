cf5cd2121d17ed6f800bf2dd3a44cc68f76cfe5b
Yeng Vang <yenghuevang@hotmail.com>
2020-05-14 09:47:46 -0500

    standard structure code for about me

--- client/src/components/AboutMeComp/AboutMeComp.js
+++ client/src/components/AboutMeComp/AboutMeComp.js
@@ -9,6 +9,15 @@ function AboutMeComp() {
   return (
     <div>
     <h1>New About Me Comp</h1>
+    <h2>Sub Heading</h2>
+    <p>Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text </p>
+    <p>Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text Text text text text </p>
+
+    <h5>Contributors</h5>
+    <p>Shawn Github </p>
+    <p>John Github</p>
+    <p>Tom Github</p>
+    <p>Yeng Github</p>
   </div>
   
     )
